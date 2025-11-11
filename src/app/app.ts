6d6a83c98fc82a52455d48ea475b2a551eda3025
WED2B <juliano.vargas@wed2b.co.uk>
2025-11-11 16:29:10 +0000

    test commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-site Hello!');
+  protected readonly title = signal('my-site Hello TESTESTE!');
 }
